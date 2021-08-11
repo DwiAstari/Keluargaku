5a7d3c5c5cb0332f5da2bdebd65502403b94481c
Dwi Astari <dwi18si@mahasiswa.pcr.ac.id>
2021-08-12 00:35:53 +0700

    PA_Keluargaku

---
+++ app/src/main/java/com/dwiastari/pa_keluargaku/KegiatanActivity.kt
@@ -0,0 +1,11 @@
+package com.dwiastari.pa_keluargaku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KegiatanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kegiatan)
+    }
+}
